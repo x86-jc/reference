46e47c4f53164d646d57060d3ef2acc9a0e21318
John Coleman <148596003+x86-jc@users.noreply.github.com>
2026-05-14 13:02:01 -0600

    Create register_size_att.s
    Register sizes reference in ATT syntax.

---
+++ register_size/register_size_att.s
@@ -0,0 +1,28 @@
+#################################################
+# Title		:	Register Sizes		#
+# Creator	:	John J. Coleman		#
+# Creation Date	:	2026.05.14		#
+# Modified Date	:	2026.05.14		#
+# 						#
+# Brief:					#
+# What can we do with different sized 		#
+# registers?					#
+#################################################
+
+.section .data
+buff:
+	.ascii "a"
+
+.section .bss
+
+.section .text
+.global _start
+_start:
+	mov $buff, %r8
+	mov (%r8), %bl
+#	mov %r8, %bl		# We cannot copy values from a larger register into a smaller register
+	mov %bl, %r8		# We can copy values from a smaller register into a larger register
+
+	mov $60, %rax
+	mov $1, %rdi
+	syscall
