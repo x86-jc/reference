26813df63428ed3be5bce8db0048cd48fca68e24
John Coleman <148596003+x86-jc@users.noreply.github.com>
2026-05-14 13:12:48 -0600

    Update register_size_att.s
    With the mov instruction the registers have to be the same size.  Here we mov byte to quad and we zero the upper bits with movzbq.

--- register_size/register_size_att.s
+++ register_size/register_size_att.s
@@ -21,7 +21,7 @@ _start:
 	mov $buff, %r8
 	mov (%r8), %bl
 #	mov %r8, %bl		# We cannot copy values from a larger register into a smaller register
-	mov %bl, %r8		# We can copy values from a smaller register into a larger register
+	movzbq %bl, %r8		# We can copy values from a smaller register into a larger register
 
 	mov $60, %rax
 	mov $1, %rdi
