d2eb200922b2b7dd3decdc0a33dbe3e99716aa5c
John Coleman <148596003+x86-jc@users.noreply.github.com>
2026-05-14 13:03:48 -0600

    Create register_sizes_intel.asm
    Register sizes reference in intel syntax.

---
+++ register_size/register_sizes_intel.asm
@@ -0,0 +1,26 @@
+;###############################################;
+; Title         :       Register Sizes          ;
+; Creator       :       John J. Coleman         ;
+; Creation Date :       2026.05.14              ;
+; Modified Date :       2026.05.14              ;
+;                                               ;
+; Brief:                                        ;
+; What can we do with different sized           ;
+; registers?                                    ;
+;###############################################;
+
+SECTION .data
+	buff:	db	"a"
+
+SECTION .bss
+
+SECTION .text
+
+	mov r8, buff
+	mov bl, [r8]
+;	mov bl, r8			; We cannot copy a larger register into a smaller register
+	mov r8, bl			; We can copy a smaller register into a larger register
+
+	mov rax, 60
+	mov rdi, 0
+	syscall
